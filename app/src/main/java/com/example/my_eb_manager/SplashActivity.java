e5395bc383858d9e46c78e2acfcf806d05bb92e6
XxCtrlZxX <harryjung1001@gmail.com>
2020-06-12 20:17:03 +0900

    Add: splash screen

--- app/src/main/java/com/example/my_eb_manager/SplashActivity.java
+++ app/src/main/java/com/example/my_eb_manager/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.my_eb_manager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +13,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 1000);
     }
 }
