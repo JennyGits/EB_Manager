c2353fd087a462dfa88e7189fb34171ac32ad8a8
XxCtrlZxX <harryjung1001@gmail.com>
2020-06-14 23:55:57 +0900

    jsoup 크롤링 테스트

--- app/src/main/java/com/example/my_eb_manager/MainActivity.java
+++ app/src/main/java/com/example/my_eb_manager/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.my_eb_manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getWebsite();
+            }
+        });
+    }
+
+    private void getWebsite() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+                try {
+                    Document doc = Jsoup.connect("https://www.naver.com").get();
+                    String title = doc.title();
+                    Elements links = doc.select("a[href]");
+
+                    builder.append(title).append("\n");
+                } catch (IOException e) {
+                    builder.append("Error");
+                }
+            }
+        }).start();
     }
 }
