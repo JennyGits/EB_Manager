1dfe34acf2ffe3ed52534b71294d1afb53166ff6
XxCtrlZxX <harryjung1001@gmail.com>
2020-06-30 19:00:44 +0900

    added WebView

--- app/src/main/java/com/example/my_eb_manager/MainActivity.java
+++ app/src/main/java/com/example/my_eb_manager/MainActivity.java
@@ -7,15 +7,28 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton floatingActionButton_add;
+
+    private WebView webView;
+    private Document doc;
 
     private RecyclerView recyclerView;
     private ListAdapter adapter;
@@ -26,8 +39,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        floatingActionButton_add = findViewById(R.id.floatingActionButton);
+        floatingActionButton_add = findViewById(R.id.floatingActionButton_add);
         recyclerView = findViewById(R.id.recycler1);
+        webView = findViewById(R.id.webView);
+
+        // WebView
+        // WebView 자바스크립트 활성화
+        webView.getSettings().setJavaScriptEnabled(true);
+        // 자바스크립트인터페이스 연결
+        // 이걸 통해 자바스크립트 내에서 자바함수에 접근할 수 있음.
+        webView.addJavascriptInterface(new MyJavascriptInterface(), "Android");
+        // 페이지가 모두 로드되었을 때, 작업 정의
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // 자바스크립트 인터페이스로 연결되어 있는 getHTML를 실행
+                // 자바스크립트 기본 메소드로 html 소스를 통째로 지정해서 인자로 넘김
+                view.loadUrl("javascript:window.Android.getHtml(document.getElementsByTagName('body')[0].innerHTML);");
+            }
+        });
+        //지정한 URL을 웹 뷰로 접근하기
+        webView.loadUrl("https://www.naver.com");
+
+
 
         // RecyclerView의 레이아웃 방식을 지정
         LinearLayoutManager manager = new LinearLayoutManager(this);
@@ -66,13 +101,22 @@ public class MainActivity extends AppCompatActivity {
 
         // 추가 버튼 누름
         floatingActionButton_add.setOnClickListener(v -> {
-            // 객체 생성
-            NotiItem notiItem = new NotiItem("누가 내 머리에 똥 쌌어");
-
-            // ListAdapter에 객체 추가
-            adapter.addItem(notiItem);
-            adapter.notifyDataSetChanged();
+            Add_NotiItem(); // 아이템 생성
         });
+    }
+
+
+
+
+
+    // 아이템 생성 함수
+    private void Add_NotiItem() {
+        // 객체 생성
+        NotiItem notiItem = new NotiItem("누가 내 머리에 똥 쌌어");
+
+        // ListAdapter에 객체 추가
+        adapter.addItem(notiItem);
+        adapter.notifyDataSetChanged();
     }
 
     private void is_LoginInfo_Exist() {
@@ -85,6 +129,30 @@ public class MainActivity extends AppCompatActivity {
             // 로그인 하기
             Toast.makeText(this, "로그인 되었습니다.", Toast.LENGTH_SHORT).show();
         }
+    }
 
+
+    public class MyJavascriptInterface {
+        @JavascriptInterface
+        public void getHtml(String html) {
+            // 위 자바스크립트가 호출되면 여기로 html이 반환됨
+            doc = StringToDocument(html);
+            // Log.d("MainActivity", "getHtml: " + html);
+            // Toast.makeText(MainActivity.this, "asdf", Toast.LENGTH_SHORT).show();
+        }
+    }
+    // String -> Document 변환하는 함수
+    private static Document StringToDocument(String xmlStr) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        try
+        {
+            builder = factory.newDocumentBuilder();
+            Document doc = builder.parse( new InputSource( new StringReader( xmlStr ) ) );
+            return doc;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
