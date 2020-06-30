90a47c638d0c443f219aafd48eabe806d5a26f30
XxCtrlZxX <harryjung1001@gmail.com>
2020-06-30 18:00:53 +0900

    added RecyclerView and icon3(gradation)

--- app/src/main/java/com/example/my_eb_manager/MainActivity.java
+++ app/src/main/java/com/example/my_eb_manager/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.my_eb_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,14 +17,61 @@ public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton floatingActionButton_add;
 
+    private RecyclerView recyclerView;
+    private ListAdapter adapter;
+    private ItemTouchHelper helper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         floatingActionButton_add = findViewById(R.id.floatingActionButton);
+        recyclerView = findViewById(R.id.recycler1);
+
+        // RecyclerView의 레이아웃 방식을 지정
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(manager);
+
+        // RecyclerView의 Adapter 세팅
+        adapter = new ListAdapter();
+        recyclerView.setAdapter(adapter);
+
+        // ItemTouchHelper 생성
+        helper = new ItemTouchHelper(new ItemTouchHelperCallback(new ItemTouchHelperListener() {
+            @Override
+            public boolean onItemMove(int from_position, int to_position) {
+                // 이동할 객체 저장
+                NotiItem notiItem = adapter.items.get(from_position);
+                // 이동할 객체 삭제
+                adapter.items.remove(from_position);
+                // 이동하고 싶은 position에 추가
+                adapter.items.add(to_position, notiItem);
+
+                // Adapter에 데이터 이동알림
+                adapter.notifyItemMoved(from_position, to_position);
+                return true;
+            }
+
+            //-- 스와이프 --//
+            @Override
+            public void onItemSwipe(int position) {
+                adapter.items.remove(position);
+                adapter.notifyItemRemoved(position);
+            }
+        }));
+        // RecyclerView 에 ItemTouchHelper 붙이기
+        helper.attachToRecyclerView(recyclerView);
+
+        // 추가 버튼 누름
         floatingActionButton_add.setOnClickListener(v -> {
-            Toast.makeText(this, "버튼 눌림", Toast.LENGTH_SHORT).show();
+            // 객체 생성
+            NotiItem notiItem = new NotiItem("누가 내 머리에 똥 쌌어");
+
+            // ListAdapter에 객체 추가
+            adapter.addItem(notiItem);
+            adapter.notifyDataSetChanged();
         });
     }
 
